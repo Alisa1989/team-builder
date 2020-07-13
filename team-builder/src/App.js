3e044c359098eed863034b0e71972ec356e66595
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-07-13 16:23:12 -0700

    working on employee displaying

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -9,27 +9,16 @@ import {useState} from 'react';
 
 
 function App() {
-  const [employees, setEmployees] = useState([]);
+  const [employees, setEmployees] = useState({name: "blank", email: "unknown", role: "N/A" });
+  
+const addEmployee = (newEmpl) => {
+  setEmployees([...employees, newEmpl])
+}
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div> 
+    <div className="App"> 
       <h1>Employees</h1>
-      <Form employees ={employees} setEmployees={setEmployees}/>
+      <Form addEmployee ={addEmployee}/>
     </div>
     
   );
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const Form = props => {
+    const [newEmployee, setNewEmployee] = useState({name:"", email:"", role:""})
+
+    const changeHandler = (event) => {
+        setNewEmployee({
+            ...newEmployee,
+            [event.target.name]: [event.target.value]
+
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            setNewEmployee({name:"", email:"", role:""})
+            console.log(props.addEmployee)
+        }}>
+
             <label>What is your name?
                 <input
+                    id= "name"
                     type = "text"
                     name = "name"
                     placeholder = "  Insert Name Here  "
+                    value ={newEmployee.name}
+                    //onChange={(event) => {setNewEmployee(event.target.value)}}
+                    onChange={changeHandler}
                     />
             </label>
             <br/>
             <label>What is your e-mail address?
                 <input
+                    id= "email"
                     type = "text"
                     name = "email"
                     placeholder = "  Insert Email Here  "
+                    value = {newEmployee.email}
+                    //onChange={(event) => {setNewEmployee(event.target.value)}}
+                    onChange={changeHandler}
                 />
             </label>
             <br/>
             <label>What is your role?
-                <input
-                    type = "text"
-                    name = "role"
-                    placeholder = "  Insert Role Here  "
-                />
+                <select id= "role" name= "role" value = {newEmployee.role} onChange={changeHandler}>
+                    <option>Please Select</option> 
+                    <option>Front End</option>
+                    <option>Back End</option>
+                    <option>Full Stack</option>
+                </select>
             </label>
             <br/>
             <button type = "submit"> Add Employee </button>
